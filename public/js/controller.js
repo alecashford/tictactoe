ce3d8c599b5ea2932d1246ff6789edfbd2d6c531
Alec Ashford <ashford91@gmail.com>
2014-09-02 15:20:04 -0700

    board set up, ready to begin integrating with the back end

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,7 +1,31 @@
 app.controller('MainController', ["$scope", "$http", function($scope, $http) {
 
-    $scope.flipCell = function() {
-        alert("done")
+    $scope.currentBoard = [[{id: 'A1', state: '?'}, {id: 'A2', state: '?'}, {id: 'A3', state: '?'}],
+                           [{id: 'B1', state: '?'}, {id: 'B2', state: '?'}, {id: 'B3', state: '?'}],
+                           [{id: 'C1', state: '?'}, {id: 'C2', state: '?'}, {id: 'C3', state: '?'}]]
+
+    $scope.toX = function(currentCell) {
+        if (currentCell.state === '?') {
+            console.log(currentCell)
+            currentCell.state = 'X'
+        }
+        $scope.toO
     }
+
+    $scope.toO = function() {
+
+    }
+
+    // $scope.setClass = function(currentCell) {
+    //     if (currentCell.x) {
+    //         return 'x'
+    //     }
+    //     else if (currentCell.o) {
+    //         return 'o'
+    //     }
+    //     else {
+    //         return 'emptyCell'
+    //     }
+    // }
 
 }]);
